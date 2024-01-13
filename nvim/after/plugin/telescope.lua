2bd593b99d00db1b9f59437fcb95d790f4261085
David <damirsa2001@gmail.com>
2024-01-13 18:49:41 +0100

    improve tmux remove file browser and find hidden

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,15 @@
 local builtin = require("telescope.builtin")
+local telescope = require('telescope')
+
+telescope.setup {
+    pickers = {
+
+	find_files = {
+
+	    hidden = true
+	}
+    }
+}
 
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
