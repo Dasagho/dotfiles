1d8021f84b83a6d18208294d6f6476efbcb79dc7
david <damirsa2001@gmail.com>
2024-02-03 18:08:13 +0100

    improve languages support

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,10 +1,20 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    formatters = {
+      --- https://github.com/sql-formatter-org/sql-formatter?tab=readme-ov-file#configuration-options
+      sql_formatter = {
+        language = "plsql", --- https://github.com/sql-formatter-org/sql-formatter/blob/master/docs/language.md
+        tabWidth = 2,
+        keywordCase = "upper",
+        linesBetweenQueries = 2,
+      },
+    },
     formatters_by_ft = {
       markdown = { "markdownlint" },
       python = { "autopep8" },
       typescript = { "ts-standard" },
+      sql = { "sql_formatter" },
     },
   },
 }
