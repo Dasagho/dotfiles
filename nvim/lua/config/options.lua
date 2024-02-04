4f8d9491a1077fa2c2542b5974c55a0d3dfd99cf
david <damirsa2001@gmail.com>
2024-02-04 23:34:52 +0100

    add db and debug support

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- vim.g.db_ui_env_variable_url = "localhost"
+-- vim.g.db_ui_env_variable_name = "postgres"
+
+vim.g.db_ui_auto_execute_table_helpers = 1
+
+vim.g.dbs = {
+  scriptura = "postgres://postgres:postgres@localhost:5432/postgres",
+}
