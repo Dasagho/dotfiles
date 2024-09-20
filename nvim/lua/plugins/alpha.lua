00faabf3cc5caf20889c45a46a05dcfd77612f4b
David Saleh <damirsa2001@gmail.com>
2024-09-20 13:26:58 +0200

    fix shortcuts alpha dashboard

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,6 +1,7 @@
 return {
     "goolord/alpha-nvim",
     -- dependencies = { 'echasnovski/mini.icons' },
+    event = 'VimEnter',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         local alpha = require('alpha')
@@ -9,13 +10,13 @@ return {
         dashboard.section.buttons.val = {
             dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
             { type = "padding", val = 1 },
-            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
             { type = "padding", val = 1 },
             dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
             { type = "padding", val = 1 },
             dashboard.button("p", "  Projects", ":Telescope projects <CR>"),
             { type = "padding", val = 1 },
-            dashboard.button("l", "󰦛  Restore session", ":Autosession search <CR>"),
+            dashboard.button("s", "󰦛  Restore session", ":Autosession search <CR>"),
             { type = "padding", val = 1 },
             dashboard.button("t", "  View TODOs", ":TodoTelescope <CR>"),
             { type = "padding", val = 1 },
