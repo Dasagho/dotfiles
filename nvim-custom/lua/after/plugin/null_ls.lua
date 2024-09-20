6805b63aaab6e3a5b82fe95049103b08589357b6
David Saleh <damirsa2001@gmail.com>
2024-09-20 13:27:08 +0200

    delete old nvim config

--- nvim-custom/lua/after/plugin/null_ls.lua
+++
@@ -1,7 +0,0 @@
-require("null-ls").setup({
-    sources = {
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.black,
-    },
-})
