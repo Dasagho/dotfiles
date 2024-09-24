a0c91c50687b8011b1bf0e35e3eec7ee4bff7316
David Saleh <damirsa2001@gmail.com>
2024-09-24 13:21:45 +0200

    neotree git status keymap

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -8,6 +8,7 @@ return {
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
-        vim.keymap.set('n', '<leader>b', ':Neotree toggle<CR>', {})
+        vim.keymap.set('n', '<leader>b', ':Neotree toggle<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>gs', ':Neotree git_status toggle<CR>', { noremap = true, silent = true, desc = "Git status" })
     end
 }
