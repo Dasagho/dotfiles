de91456fb5756d578115f82ea50897e24d473aef
David <damirsa2001@gmail.com>
2024-12-08 16:41:17 +0100

    add kitty config and scripted

--- scripts/install_last_version.sh
+++ scripts/install_last_version.sh
@@ -11,6 +11,7 @@ mkdir -p "$INSTALL_DIRECTORY/kitty" && tar xvf "$INSTALL_DIRECTORY/kitty.txz" --
 ln -sf "$INSTALL_DIRECTORY/kitty/bin/kitty" "$BIN_DIRECTORY/kitty"
 cp ./desktop/kitty/kitty.desktop $INSTALL_DIRECTORY/applications
 cp ./desktop/kitty/icon/kitty.png $INSTALL_DIRECTORY/icons/hicolor/256x256/apps/
+cp -r ../config/kitty $CONFIG_DIRECTORY
 
 # Install fish
 last_fish_version=$(curl https://api.github.com/repos/fish-shell/fish-shell/releases/latest | jq -r .name | awk '{print $2}')
@@ -25,3 +26,5 @@ fish --command "omf install bobthefish"
 
 cp -r ../config/fish $CONFIG_DIRECTORY
 chmod +x -R $BIN_DIRECTORY
+
+cp -r ../fonts/* "$INSTALL_DIRECTORY/fonts"
