6de72bdc7bab8c98b53ab9a1992a39a901cb343d
David <damirsa2001@gmail.com>
2024-12-08 16:17:02 +0100

    add script to install kitty and fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,59 +1,40 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias ls="exa"
-    alias ll="exa -l --icons --header"
-    alias cat="batcat"
-    alias cat="batcat"
-    alias nubes="cd ~/svn/dialapplet-clients"
-    alias tags="cd ~/svn/DIALAPPLET/tags"
-    alias dck="cd ~/svn/tecsible/dialapplet-docker-desarrollo"
+    # alias ls="exa"
+    # alias ll="exa -l --icons --header"
+    # alias cat="batcat"
     alias dps='docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}"'
-    alias dup="docker-compose up -d"
     alias sfish="source ~/.config/fish/config.fish"
-    alias kubectl="minikube kubectl --"
-    alias containers='docker ps --format "{{.Names}}" | fzf | xargs -o -I {} docker exec -it {} /bin/bash'
+    # alias kubectl="minikube kubectl --"
+    alias dconn='docker ps --format "{{.Names}}" | fzf | xargs -o -I {} docker exec -it {} /bin/bash'
 
     fish_add_path -g /home/ayuda104/Scripts
     set -g theme_nerd_fonts yes
     set -g theme_color_scheme dark
     set -g theme_display_git_dirty yes
     set -g theme_display_git_untracked yes
-    fish_add_path "/home/ayuda104/.local/share/fnm"
+    # fish_add_path "/home/ayuda104/.local/share/fnm"
 
-    set -Ux FLYCTL_INSTALL "/home/ayuda104/.fly"
-    fish_add_path "$FLYCTL_INSTALL/bin"
-    fish_add_path $HOME/.local/share/nvim/mason/bin
+    # set -Ux FLYCTL_INSTALL "/home/ayuda104/.fly"
+    # fish_add_path "$FLYCTL_INSTALL/bin"
+    # fish_add_path $HOME/.local/share/nvim/mason/bin
 
     if test -n "$TMUX"
         set -x TERM xterm-256color
     end
 
-    set branch (readlink $HOME/currentProject | sed 's|.*/DIALAPPLET/||')
-    echo -n "Working on branch: "
-    set_color cyan
-    echo $branch
-    set_color normal
-    dialstatus dialserver
-    dialstatus dialcontact
-    dialstatus dialreport
-    # set -g SDKMAN_DIR "$HOME/.sdkman"
-    # source "$HOME/.sdkman/bin/sdkman-init.sh"
-    set -g __sdkman_custom_dir "$HOME/.sdkman"
-    fnm env | source
+    # set -g __sdkman_custom_dir "$HOME/.sdkman"
+    # fnm env | source
 end
 
-# Remap suspend action to Ctrl+Y
-stty susp \x19
-# Disable Ctrl+Z
-stty susp undef
 
 # bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+# set --export BUN_INSTALL "$HOME/.bun"
+# set --export PATH $BUN_INSTALL/bin $PATH
 
 # pnpm
-set -gx PNPM_HOME "/home/ayuda104/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
+# set -gx PNPM_HOME "/home/ayuda104/.local/share/pnpm"
+# if not string match -q -- $PNPM_HOME $PATH
+#   set -gx PATH "$PNPM_HOME" $PATH
+# end
 # pnpm end
--- scripts/install_last_version.sh
+++ scripts/install_last_version.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
 BIN_DIRECTORY=$HOME/.local/bin
+INSTALL_DIRECTORY=$HOME/.local/share
+CONFIG_DIRECTORY=$HOME/.config
 
+# Install kitty
 last_kitty_version=$(curl https://api.github.com/repos/kovidgoyal/kitty/releases/latest | jq -r .name | awk '{print $2}')
-wget "https://github.com/kovidgoyal/kitty/releases/download/v$last_kitty_version/kitten-linux-amd64" -O $BIN_DIRECTORY/kitty
+wget "https://github.com/kovidgoyal/kitty/releases/download/v$last_kitty_version/kitty-$last_kitty_version-x86_64.txz" -O "$INSTALL_DIRECTORY/kitty.txz"
+mkdir -p "$INSTALL_DIRECTORY/kitty" && tar xvf "$INSTALL_DIRECTORY/kitty.txz" --directory "$INSTALL_DIRECTORY/kitty"
+ln -sf "$INSTALL_DIRECTORY/kitty/bin/kitty" "$BIN_DIRECTORY/kitty"
+cp ./desktop/kitty/kitty.desktop $INSTALL_DIRECTORY/applications
+cp ./desktop/kitty/icon/kitty.png $INSTALL_DIRECTORY/icons/hicolor/256x256/apps/
 
+# Install fish
+last_fish_version=$(curl https://api.github.com/repos/fish-shell/fish-shell/releases/latest | jq -r .name | awk '{print $2}')
+wget "https://github.com/fish-shell/fish-shell/releases/download/$last_fish_version/fish-$last_fish_version.app.zip" -O "$INSTALL_DIRECTORY/fish.zip"
+rm "$INSTALL_DIRECTORY/fish.zip" && unzip "$INSTALL_DIRECTORY/fish.zip"
+ln -sf "$INSTALL_DIRECTORY/fish.app/Contents/Resources/base/usr/local/bin/fish" "$BIN_DIRECTORY/fish"
+
+# Install ohmyfish
+rm -rf "$INSTALL_DIRECTORY/omf"
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+fish --command "omf install bobthefish"
+
+cp -r ../config/fish $CONFIG_DIRECTORY
 chmod +x -R $BIN_DIRECTORY
