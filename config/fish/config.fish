14103751a7305193f619ba41d75b8b116ef635a9
David Saleh <damirsa2001@gmail.com>
2025-01-15 14:34:45 +0100

    kitty find or exec

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,28 +7,31 @@ if status is-interactive
     alias sfish="source ~/.config/fish/config.fish"
     # alias kubectl="minikube kubectl --"
     alias dconn='docker ps --format "{{.Names}}" | fzf | xargs -o -I {} docker exec -it {} /bin/bash'
+    alias kitty='kitty --title "kitty"'
 
     fish_add_path -g /home/ayuda104/Scripts
     set -g theme_nerd_fonts yes
     set -g theme_color_scheme dark
     set -g theme_display_git_dirty yes
     set -g theme_display_git_untracked yes
+    set -g theme_display_python_virtualenv no
+
     # fish_add_path "/home/ayuda104/.local/share/fnm"
 
     # set -Ux FLYCTL_INSTALL "/home/ayuda104/.fly"
     # fish_add_path "$FLYCTL_INSTALL/bin"
-    # fish_add_path $HOME/.local/share/nvim/mason/bin
+    fish_add_path $HOME/.local/share/nvim/mason/bin
     fish_add_path $HOME/.local/bin
 
     if test -n "$TMUX"
         set -x TERM xterm-256color
     end
-    source ~/.config/fish/functions/git_alias.fish
 
-    # set -g __sdkman_custom_dir "$HOME/.sdkman"
-    # fnm env | source
+    set -g __sdkman_custom_dir "$HOME/.sdkman"
+    fnm env | source
 end
 
+# source ~/.config/fish/functions/git_alias.fish
 
 # bun
 # set --export BUN_INSTALL "$HOME/.bun"
