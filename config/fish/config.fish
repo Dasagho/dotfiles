ff621eb93998f18b9d7a96d9c96e90894bf3922b
David <damirsa2001@gmail.com>
2025-01-26 14:40:48 +0100

    config bobthefish theme

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,12 +9,20 @@ if status is-interactive
     alias dconn='docker ps --format "{{.Names}}" | fzf | xargs -o -I {} docker exec -it {} /bin/bash'
     alias kitty='kitty --title "kitty"'
 
-    fish_add_path -g /home/ayuda104/Scripts
+    # fish_add_path -g /home/ayuda104/Scripts
     set -g theme_nerd_fonts yes
-    set -g theme_color_scheme dark
+    set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+    set -g theme_color_scheme dracula
     set -g theme_display_git_dirty yes
     set -g theme_display_git_untracked yes
     set -g theme_display_python_virtualenv no
+    set -g theme_display_node yes
+    set -g theme_title_use_abbreviated_path no
+    set -g theme_display_git_dirty_verbose yes
+    set -g theme_display_git_ahead_verbose yes
+    set -g theme_display_git_default_branch yes
+    set -g theme_date_format +%c
+    set -g theme_date_format +%H:%M:%S
 
     # fish_add_path "/home/ayuda104/.local/share/fnm"
 
