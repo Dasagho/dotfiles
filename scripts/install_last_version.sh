d95f2e9e9810e9aca67591f79b97e958d59f7f59
David <damirsa2001@gmail.com>
2025-01-29 10:48:58 +0100

    improve install script with make directories

--- scripts/install_last_version.sh
+++ scripts/install_last_version.sh
@@ -21,6 +21,8 @@ BIN_DIRECTORY=$HOME/.local/bin
 INSTALL_DIRECTORY=$HOME/.local/share
 CONFIG_DIRECTORY=$HOME/.config
 
+mkdir "$BIN_DIRECTORY"
+
 # Install kitty
 last_kitty_version=$(curl https://api.github.com/repos/kovidgoyal/kitty/releases/latest | jq -r .name | awk '{print $2}')
 wget "https://github.com/kovidgoyal/kitty/releases/download/v$last_kitty_version/kitty-$last_kitty_version-x86_64.txz" -O "$INSTALL_DIRECTORY/kitty.txz"
@@ -30,17 +32,24 @@ cp ./desktop/kitty/kitty.desktop $INSTALL_DIRECTORY/applications
 cp ./desktop/kitty/icon/kitty.png $INSTALL_DIRECTORY/icons/hicolor/256x256/apps/
 cp -r ../config/kitty $CONFIG_DIRECTORY
 
+# Install fnm
+curl -fsSL https://fnm.vercel.app/install | bash
+
 # Install tealdeer
 last_tldr_version=$(curl https://api.github.com/repos/tealdeer-rs/tealdeer/releases/latest | jq -r .name | awk '{print $2}')
 wget "https://github.com/tealdeer-rs/tealdeer/releases/download/v$last_tldr_version/tealdeer-linux-x86_64-musl" -O "$BIN_DIRECTORY/tldr"
 chmod +x "$BIN_DIRECTORY/tldr"
 wget "https://github.com/tealdeer-rs/tealdeer/releases/download/v$last_tldr_version/completions_fish" -O "$CONFIG_DIRECTORY/fish/functions"
 
+# Install tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # Config fish
 cp -r ../config/fish $CONFIG_DIRECTORY
 chmod +x -R $BIN_DIRECTORY
 
 # Install fonts
+mkdir "$INSTALL_DIRECTORY/fonts"
 cp ../fonts/* "$INSTALL_DIRECTORY/fonts"
 fc-cache
 
