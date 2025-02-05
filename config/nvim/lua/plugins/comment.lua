e41067bca1a072a8471cc129a5d171f1ebc0226d
David <damirsa2001@gmail.com>
2025-02-05 10:47:52 +0100

    add comment jsx support

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,10 +1,15 @@
 return {
   "numToStr/Comment.nvim",
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
   opts = {
     -- add any options here
   },
   event = { "BufReadPost", "BufNewFile" },
   config = function()
-    require("Comment").setup()
+    require("Comment").setup {
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    }
   end,
 }
