8d019299940845e14ddca0c90b9b5b1fe90ea656
David <damirsa2001@gmail.com>
2025-02-24 20:48:44 +0100

    fix: fish path declarations

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,49 +1,45 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # alias ls="exa"
-    # alias ll="exa -l --icons --header"
-    # alias cat="batcat"
-    alias dps='docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}"'
-    alias sfish="source ~/.config/fish/config.fish"
-    # alias kubectl="minikube kubectl --"
-    alias dconn='docker ps --format "{{.Names}}" | fzf | xargs -o -I {} docker exec -it {} /bin/bash'
-    alias kitty='kitty --title "kitty"'
+  # Commands to run in interactive sessions can go here
+  # alias ls="exa"
+  # alias ll="exa -l --icons --header"
+  # alias cat="batcat"
+  alias dps='docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}"'
+  alias sfish="source ~/.config/fish/config.fish"
+  # alias kubectl="minikube kubectl --"
+  alias dconn='docker ps --format "{{.Names}}" | fzf | xargs -o -I {} docker exec -it {} /bin/bash'
+  alias kitty='kitty --title "kitty"'
 
-    # fish_add_path -g /home/ayuda104/Scripts
-    set -g theme_nerd_fonts yes
-    set -x VIRTUAL_ENV_DISABLE_PROMPT 1
-    set -g theme_color_scheme dracula
-    set -g theme_display_git_dirty yes
-    set -g theme_display_git_untracked yes
-    set -g theme_display_python_virtualenv no
-    set -g theme_display_node yes
-    set -g theme_title_use_abbreviated_path no
-    set -g theme_display_git_dirty_verbose yes
-    set -g theme_display_git_ahead_verbose yes
-    set -g theme_display_git_default_branch yes
-    set -g theme_date_format +%c
-    set -g theme_date_format +%H:%M:%S
-    set -g theme_use_abbreviated_branch_name yes
-    set -g theme_display_git yes
+  # set -gx PATH $HOME/Scripts $PATH
+  set -g theme_nerd_fonts yes
+  set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+  set -g theme_color_scheme dracula
+  set -g theme_display_git_dirty yes
+  set -g theme_display_git_untracked yes
+  set -g theme_display_python_virtualenv no
+  set -g theme_display_node yes
+  set -g theme_title_use_abbreviated_path no
+  set -g theme_display_git_dirty_verbose yes
+  set -g theme_display_git_ahead_verbose yes
+  set -g theme_display_git_default_branch yes
+  set -g theme_date_format +%c
+  set -g theme_date_format +%H:%M:%S
+  set -g theme_use_abbreviated_branch_name yes
+  set -g theme_display_git yes
 
-    # fish_add_path "/home/ayuda104/.local/share/fnm"
+  # set -Ux FLYCTL_INSTALL "$HOME/.fly"
+  # set -gx PATH "$FLYCTL_INSTALL/bin" $PATH
+  set -gx PATH  "$HOME/.local/share/nvim/mason/bin" $PATH
 
-    # set -Ux FLYCTL_INSTALL "/home/ayuda104/.fly"
-    # fish_add_path "$FLYCTL_INSTALL/bin"
-    fish_add_path $HOME/.local/share/nvim/mason/bin
-    fish_add_path $HOME/.local/bin
+  set -g __sdkman_custom_dir "$HOME/.sdkman"
+  set -gx PATH "$HOME/.sdkman/candidates/maven/current/bin" $PATH
+  set -gx PATH "$HOME/.sdkman/candidates/java/current/bin" $PATH
+  set -gx PATH "$HOME/.sdkman/candidates/springboot/current/bin" $PATH
+  if not contains "$HOME/.local/bin" $PATH
+    set -gx PATH "$HOME/.local/bin" $PATH
+  end
 
-    if test -n "$TMUX"
-        set -x TERM xterm-256color
-    end
-
-    set -g __sdkman_custom_dir "$HOME/.sdkman"
-    fish_add_path "$HOME/.sdkman/candidates/maven/current/bin"
-    fish_add_path "$HOME/.sdkman/candidates/java/current/bin"
-    fish_add_path "$HOME/.sdkman/candidates/springboot/current/bin"
-    fnm env | source
+  fnm env | source
 end
-
 # source ~/.config/fish/functions/git_alias.fish
 
 # bun
@@ -51,7 +47,7 @@ end
 # set --export PATH $BUN_INSTALL/bin $PATH
 
 # pnpm
-set -gx PNPM_HOME "/home/ayuda104/.local/share/pnpm"
+set -gx PNPM_HOME "/home/david/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
