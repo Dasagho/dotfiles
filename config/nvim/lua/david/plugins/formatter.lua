1527ba395642b73254626467b096d171a5cdfbb5
David Saleh <david.amir@dialapplet.com>
2025-06-26 09:21:38 +0200

    fix: prettier detect file configuration

--- config/nvim/lua/david/plugins/formatter.lua
+++ config/nvim/lua/david/plugins/formatter.lua
@@ -2,41 +2,37 @@ return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
-  keys = {
-    {
-      '<leader>f',
-      function()
-        require('conform').format { async = true, lsp_format = 'fallback' }
-      end,
-      mode = '',
-      desc = '[F]ormat buffer',
-    },
-  },
-  config = function()
-    require('conform').setup {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        python = { 'isort', 'black' },
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'prettierd', 'prettier', stop_after_first = true },
-        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+  opts = function(_, opts)
+    opts.notify_on_error = false
+
+    opts.format_on_save = function(bufnr)
+      local disable = { c = true, cpp = true }
+      if disable[vim.bo[bufnr].filetype] then
+        return nil
+      end
+      return { timeout_ms = 500, lsp_format = 'fallback' }
+    end
+
+    opts.formatters_by_ft = vim.tbl_deep_extend('force', opts.formatters_by_ft or {}, {
+      lua = { 'stylua' },
+      python = { 'isort', 'black' },
+      javascript = { 'prettierd', stop_after_first = true },
+      typescript = { 'prettierd', stop_after_first = true },
+      javascriptreact = { 'prettierd', stop_after_first = true },
+      typescriptreact = { 'prettierd', stop_after_first = true },
+    })
+
+    opts.formatters = opts.formatters or {}
+    opts.formatters.prettierd = {
+      require_cwd = true,
+      cwd = require('conform.util').root_file {
+        'prettierrc.json',
+        '.prettierrc',
+        '.prettierrc.json',
+        'prettier.config.js',
+        'package.json',
       },
+      prepend_args = { '--config', vim.fn.getcwd() .. '/prettierrc.json' },
     }
   end,
 }
